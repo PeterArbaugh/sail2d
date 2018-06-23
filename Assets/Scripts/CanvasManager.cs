da5f69dc179586907da514b18f0e482a48a28a0d
PeterArbaugh <PeterArbaugh@users.noreply.github.com>
2018-06-23 16:27:47 -0400

    Create prefabs and new scenes

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour {
+
+    //Irons Indicator
+    public GameObject irons;
+    Animator ironsAnim;
+    public GameObject player;
+    public bool status;
+
+
+	// Use this for initialization
+	void Start () {
+        //Irons indicator
+        ironsAnim = irons.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Irons indicator
+        status = player.GetComponent<BoatMovement>().irons;
+
+        if (status)
+        {
+            ironsAnim.SetBool("InIrons", true);
+        }
+        if (!status)
+        {
+            ironsAnim.SetBool("InIrons", false);
+        }
+		
+	}
+}
---
+++ Assets/Scripts/GameOverScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScene : MonoBehaviour {
+
+    [SerializeField]
+    Button restart;
+    Button exit;
+
+    void Start()
+    {
+        restart.onClick.AddListener(RestartScene);
+        exit.onClick.AddListener(ExitScene);
+    }
+
+    // Update is called once per frame
+	void Update () {
+		
+	}
+
+    void RestartScene()
+    {
+        //SceneManager.LoadScene("");
+    }
+
+    void ExitScene()
+    {
+        Application.Quit();
+    }
+}
